242778cdac558cad7eb88c2445ce621ceb11eb75
kozmer <87979263+kozmer@users.noreply.github.com>
2024-09-20 13:59:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,17 @@ from setuptools import setup
 
 setup(
     name="kerberos-keygen",
-    version="0.1",
-    author="kozmer",
-    author_email="kozmer@protonmail.com",
+    version="1.0",
     description="A tool to calculate Kerberos keys for AD accounts",
-    py_modules=["kerberos_keygen"],  # This refers to kerberos_keygen.py
+    py_modules=["kerberos_keygen"],
     install_requires=[
         "impacket",
         "pycryptodome",
     ],
     entry_points={
         'console_scripts': [
-            'kerberos-keygen=kerberos_keygen:main',  # Point to kerberos_keygen.py's main() function
+            'kerberos-keygen=kerberos_keygen:main',
         ],
     },
     python_requires='>=3.10',
 )
-
